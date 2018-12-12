0d46c0c8fdc9d1fcfccc8a33b9e767acdb1649cb
lilu <lilugirl2005@126.com>
2018-12-13 00:18:47 +0800

    init

---
+++ functional-playground/play.js
@@ -0,0 +1,28 @@
+import {
+  forEach,
+  forEachObject,
+  unless,
+  times
+} from '../lib/es6-functional'
+
+var array = [1, 2, 3]
+forEach(array, (data) => console.log(data))
+
+let object = {
+  a: 1,
+  b: 2
+}
+forEachObject(object, (k, v) => console.log(k + ":" + v))
+
+forEach([1, 2, 3, 4, 5, 6, 7], (number) => {
+  unless((number % 2), () => {
+    console.log(number, " is even")
+  })
+})
+
+
+times(100, function (n) {
+  unless(n % 2, function () {
+    console.log(n, " is even");
+  });
+});
