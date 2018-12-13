c1f4989ac1e31842bf64ba38d5f80df210909cfb
lilu <lilugirl2005@126.com>
2018-12-14 00:31:11 +0800

    真实的高阶函数

--- functional-playground/play.js
+++ functional-playground/play.js
@@ -2,7 +2,10 @@ import {
   forEach,
   forEachObject,
   unless,
-  times
+  times,
+  every,
+  some,
+  sortBy
 } from '../lib/es6-functional'
 
 var array = [1, 2, 3]
@@ -21,8 +24,50 @@ forEach([1, 2, 3, 4, 5, 6, 7], (number) => {
 })
 
 
-times(100, function (n) {
+times(10, function (n) {
   unless(n % 2, function () {
     console.log(n, " is even");
   });
 });
+
+console.log(every([NaN, NaN, NaN], isNaN));
+console.log(every([NaN, NaN, 4], isNaN));
+
+console.log(some([NaN, NaN, 4], isNaN));
+console.log(some([4, 3, 2], isNaN));
+
+var fruit = ['cherries', 'apples', 'bananas'];
+console.log(fruit.sort());
+
+var people = [{
+    firstname: 'aaFirstName',
+    lastname: 'cclastName'
+  },
+  {
+    firstname: 'ccFirstName',
+    lastname: 'aalastName'
+  },
+  {
+    firstname: 'bbFirstName',
+    lastname: 'bblastName'
+  },
+];
+
+people.sort((a, b) => {
+  return (a.firstname < b.firstname) ? -1 : (a.firstname > b.firstname) ? 1 : 0
+})
+
+console.log(people);
+
+people.sort((a, b) => {
+  return (a.lastname < b.lastname) ? -1 : (a.lastname > b.lastname) ? 1 : 0
+})
+
+console.log(people);
+
+
+people.sort(sortBy("firstname"));
+console.log(people);
+
+people.sort(sortBy('lastname'));
+console.log(people);
