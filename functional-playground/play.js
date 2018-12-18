6097999631df4717f510102a18e6b51b283f57fb
lilu <lilugirl2005@126.com>
2018-12-19 02:22:05 +0800

    真实高阶函数(续)

--- functional-playground/play.js
+++ functional-playground/play.js
@@ -5,7 +5,13 @@ import {
   times,
   every,
   some,
-  sortBy
+  sortBy,
+  sortBy2,
+  sortBy3,
+  tap,
+  unary,
+  once,
+  memoized
 } from '../lib/es6-functional'
 
 var array = [1, 2, 3]
@@ -41,15 +47,18 @@ console.log(fruit.sort());
 
 var people = [{
     firstname: 'aaFirstName',
-    lastname: 'cclastName'
+    lastname: 'cclastName',
+    time: 123456,
   },
   {
     firstname: 'ccFirstName',
-    lastname: 'aalastName'
+    lastname: 'aalastName',
+    time: 234566,
   },
   {
     firstname: 'bbFirstName',
-    lastname: 'bblastName'
+    lastname: 'bblastName',
+    time: 334566,
   },
 ];
 
@@ -71,3 +80,58 @@ console.log(people);
 
 people.sort(sortBy('lastname'));
 console.log(people);
+
+people.sort(sortBy2('time', 'desc'));
+console.log(people);
+
+people.sort(sortBy2('time', 'asc'));
+console.log(people);
+
+people.sort(sortBy3('time', 'asc'));
+console.log(people);
+
+people.sort(sortBy3('time', 'desc'));
+console.log(people);
+
+
+tap("fun")((it) => console.log("value is ", it));
+
+forEach([1, 2, 3], (a) => tap(a)(() => {
+  console.log(a)
+}));
+
+
+const result = [1, 2, 3].map((a) => {
+  return a * a
+});
+
+
+
+console.log(result);
+
+const result2 = ['1', '2', '3'].map(unary(parseInt));
+console.log(result2);
+
+var doPayment = once(() => {
+  console.log('Payment is done')
+});
+
+doPayment();
+
+
+doPayment();
+
+
+let fastFactorial = memoized((n) => {
+  if (n === 0) {
+    return 1;
+  }
+
+  return n * fastFactorial(n - 1);
+});
+
+const result3 = fastFactorial(5);
+console.log(result3);
+
+const result4 = fastFactorial(4);
+console.log(result4);
